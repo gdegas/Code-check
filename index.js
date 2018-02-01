76832f078530dcb1eb42f0f867270b750f1043fc
gdegas001 <gdegas001@gmail.com>
2018-01-31 20:21:21 -0800

    Changed console log structure

--- index.js
+++ index.js
@@ -27,25 +27,42 @@ function getTodos(url) {
 // })
 const url = 'https://jsonplaceholder.typicode.com';
 
-const getLocation = async url => {
+const brettsData = async (url, userId) => {
   try {
-    const todosResponse = await fetch(url + '/todos?userId=1');
+    const usersResponse = await fetch(url + '/users/' + userId)
+    const userJson = await usersResponse.json();
+    const todosResponse = await fetch(url + '/todos?userId=' + userId);
     const todosJson = await todosResponse.json();
-    const albumsResponse = await fetch(url + '/albums?userId=1')
+    const albumsResponse = await fetch(url + '/albums?userId=' + userId)
     const albumsJson = await albumsResponse.json()
-    const postsResponse = await fetch(url + '/posts?userId=1')
+    const postsResponse = await fetch(url + '/posts?userId=' + userId)
     const postsJson = await postsResponse.json()
+    // const post1Response = await fetch(url + '/posts/1')
+    // const post1Json = await post1Response.json()
+    // const post2Response = await fetch(url + '/posts/2')
+    // const post2Json = await postsResponse.json()
+    // const post3Response = await fetch(url + '/posts/3')
+    // const post3Json = await postsResponse.json()
+    // const post4Response = await fetch(url + '/posts/4')
+    // const post4Json = await postsResponse.json()
+    // const post5Response = await fetch(url + '/posts/5')
+    // const post5Json = await postsResponse.json()
     console.log(
-      `Todos: ${todosJson.length} -`,
-      `Albums: ${albumsJson.length} -`,
-      `Posts: ${postsJson.length}`
+      `${userJson.username} has ${postsJson.length} posts, ${albumsJson.length} albums, and ${todosJson.length} todos`,
+      `post 1: ${postsJson[0].title}`,
+      `post 2: ${postsJson[1].title}`,
+      `post 3: ${postsJson[2].title}`,
+      `post 4: ${postsJson[3].title}`,
+      `post 5: ${postsJson[4].title}`,
+      
+
     );
   } catch (error) {
     console.log(error);
   }
 };
 
-getLocation(url);
+brettsData(url, 1);
 
   
 
